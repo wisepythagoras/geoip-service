674e08d7f974c1d3843b1efd0f30282ed90ba2b9
wisepythagoras <55108282+wisepythagoras@users.noreply.github.com>
2020-05-10 13:00:51 -0400

    Update download script

--- tools/update_geolite.sh
+++ tools/update_geolite.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-curl http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz --output GeoLite2-City.tar.gz
-curl http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz --output GeoLite2-Country.tar.gz
-curl http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz --output GeoLite2-ASN.tar.gz
-
-tar xf GeoLite2-City.tar.gz
-tar xf GeoLite2-Country.tar.gz
-tar xf GeoLite2-ASN.tar.gz
+tar xf GeoLite2-City_$(date +%Y)*.tar.gz
+tar xf GeoLite2-Country_$(date +%Y)*.tar.gz
+tar xf GeoLite2-ASN_$(date +%Y)*.tar.gz
 
 cp GeoLite2-*_$(date +%Y)*/*.mmdb geolite/
 
