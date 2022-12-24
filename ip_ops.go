70e27602a3ec9ba6a7a86f746142daa01216e990
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2022-12-24 16:12:15 -0500

    Update ip ops file

--- ip_ops.go
+++ ip_ops.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-    "strings"
-    "regexp"
-    "net"
+	"net"
+	"regexp"
+	"strings"
 )
 
 func IsIPv6(str string) bool {
-    ip := net.ParseIP(str)
-    return ip != nil && strings.Contains(str, ":")
+	ip := net.ParseIP(str)
+	return ip != nil && strings.Contains(str, ":")
 }
 
 func IsValidIP(ipAddress string) bool {
-    ipAddress = strings.Trim(ipAddress, " ")
+	ipAddress = strings.Trim(ipAddress, " ")
 
-    re, _ := regexp.Compile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`)
+	re, _ := regexp.Compile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`)
 
-    if re.MatchString(ipAddress) {
-        return true
-    }
+	if re.MatchString(ipAddress) {
+		return true
+	}
 
-    return IsIPv6(ipAddress)
+	return IsIPv6(ipAddress)
 }
-
