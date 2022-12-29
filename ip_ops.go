c5ada52e45c99f829bad6a2a7742526ec0fb0b70
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2022-12-29 16:44:54 -0500

    Move the ParseIPList to the utils file

--- ip_ops.go
+++ ip_ops.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"net"
-	"os"
 	"regexp"
 	"strings"
 )
@@ -23,44 +21,4 @@ func IsValidIP(ipAddress string) bool {
 	}
 
 	return IsIPv6(ipAddress)
-}
-
-// ParseIPList parses a list of IP addresses and CIDR ranges into two lists which could be used
-// as a whitelist or blacklist.
-func ParseIPList(file *os.File) ([]*net.IPNet, []net.IP, error) {
-	scanner := bufio.NewScanner(file)
-	ipRanges := []*net.IPNet{}
-	ipAddresses := []net.IP{
-		net.ParseIP("127.0.0.1"),
-	}
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		re := regexp.MustCompile(`(#[\s\S]+)`)
-		line = strings.Trim(re.ReplaceAllLiteralString(line, ""), " ")
-
-		if len(line) == 0 {
-			continue
-		}
-
-		isCidrRange := strings.Contains(line, "/")
-
-		if isCidrRange {
-			_, ipRange, err := net.ParseCIDR(line)
-
-			if err != nil {
-				return nil, nil, err
-			}
-
-			ipRanges = append(ipRanges, ipRange)
-		} else {
-			ipAddresses = append(ipAddresses, net.ParseIP(line))
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, nil, err
-	}
-
-	return ipRanges, ipAddresses, nil
 }
--- utils.go
+++ utils.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"net"
+	"os"
 	"reflect"
+	"regexp"
+	"strings"
 )
 
 func sliceContains[T any](arr []T, thing T) bool {
@@ -12,4 +17,44 @@ func sliceContains[T any](arr []T, thing T) bool {
 	}
 
 	return false
+}
+
+// ParseIPList parses a list of IP addresses and CIDR ranges into two lists which could be used
+// as a whitelist or blacklist.
+func ParseIPList(file *os.File) ([]*net.IPNet, []net.IP, error) {
+	scanner := bufio.NewScanner(file)
+	ipRanges := []*net.IPNet{}
+	ipAddresses := []net.IP{
+		net.ParseIP("127.0.0.1"),
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		re := regexp.MustCompile(`(#[\s\S]+)`)
+		line = strings.Trim(re.ReplaceAllLiteralString(line, ""), " ")
+
+		if len(line) == 0 {
+			continue
+		}
+
+		isCidrRange := strings.Contains(line, "/")
+
+		if isCidrRange {
+			_, ipRange, err := net.ParseCIDR(line)
+
+			if err != nil {
+				return nil, nil, err
+			}
+
+			ipRanges = append(ipRanges, ipRange)
+		} else {
+			ipAddresses = append(ipAddresses, net.ParseIP(line))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, nil, err
+	}
+
+	return ipRanges, ipAddresses, nil
 }
