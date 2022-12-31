ee39657a607f6d5cee2113bd576974a107d87c4e
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2022-12-31 14:41:01 -0500

    Add city name

--- types.go
+++ types.go
@@ -2,12 +2,18 @@ package main
 
 type IPRecord struct {
 	Country struct {
-		ISOCode   string `maxminddb:"iso_code" json:"iso_code"`
-		GeonameID int    `maxminddb:"geoname_id" json:"geoname_id"`
+		ISOCode   string            `maxminddb:"iso_code" json:"iso_code"`
+		GeonameID int               `maxminddb:"geoname_id" json:"geoname_id"`
+		Name      map[string]string `maxminddb:"names" json:"name"`
 	} `maxminddb:"country" json:"country"`
+	City struct {
+		GeonameID int               `maxminddb:"geoname_id" json:"geoname_id"`
+		Name      map[string]string `maxminddb:"names" json:"name"`
+	} `maxminddb:"city" json:"city"`
 	Location struct {
 		Latitude  float32 `maxminddb:"latitude" json:"latitude"`
 		Longitude float32 `maxminddb:"longitude" json:"longitude"`
+		MetroCode int     `maxminddb:"metro_code" json:"metro_code"`
 	} `maxminddb:"location" json:"location"`
 	ASN       int    `maxminddb:"autonomous_system_number" json:"asn"`
 	Org       string `maxminddb:"autonomous_system_organization" json:"org"`
