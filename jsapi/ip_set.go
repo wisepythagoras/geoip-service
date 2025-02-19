3953adab68bed4f112b842c5a823dba6ef7ed8f4
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2025-02-19 17:34:03 -0500

    Implement contains in IPSet

--- jsapi/ip_set.go
+++ jsapi/ip_set.go
@@ -2,9 +2,31 @@ package jsapi
 
 import (
 	"fmt"
+	"net"
+	"strings"
+	"time"
 
 	js "github.com/dop251/goja"
 )
+
+const (
+	UPDATE_HOURLY   = 0
+	UPDATE_DAILY    = 1
+	UPDATE_WEEKLY   = 2
+	UPDATE_BIWEEKLY = 3
+	UPDATE_MONTHLY  = 4
+)
+
+type ipSetOpts struct {
+	Name        string    `json:"name"`
+	Maintainer  string    `json:"maintainer"`
+	URL         string    `json:"url"`
+	Date        time.Time `json:"date"`
+	UpdateFreq  uint      `json:"update_freq"`
+	Version     uint64    `json:"version"`
+	Description string    `json:"description"`
+	Notes       string    `json:"notes"`
+}
 
 type JSIPSet struct {
 	VM    *js.Runtime
@@ -25,11 +47,20 @@ func (ip *JSIPSet) constructor(call js.ConstructorCall) *js.Object {
 	}
 
 	var err error
-	ipAddrsInterface := call.Argument(0).Export()
-	ipAddrs, ok := ipAddrsInterface.([]string)
+	var opts ipSetOpts
 
-	if !ok {
-		ipAddrs, err = parseSimpleIPList(call.Argument(0).String())
+	entries := make([]string, 0)
+	err = ip.VM.ExportTo(call.Argument(0), &entries)
+	ipAddrsMap := make(map[string]net.IP)
+	cidrEntries := make([]*net.IPNet, 0)
+	cidrEntryMap := make(map[string]*net.IPNet)
+
+	if len(call.Arguments) > 1 {
+		ip.VM.ExportTo(call.Argument(1), &opts)
+	}
+
+	if err != nil {
+		entries, err = parseSimpleIPList(call.Argument(0).String())
 
 		if err != nil {
 			ip.VM.Interrupt(err)
@@ -37,9 +68,68 @@ func (ip *JSIPSet) constructor(call js.ConstructorCall) *js.Object {
 		}
 	}
 
-	fmt.Println(ipAddrs)
+	for _, entry := range entries {
+		if strings.Contains(entry, "/") {
+			_, cidrEntry, err := net.ParseCIDR(entry)
+
+			if err != nil {
+				ip.VM.Interrupt(err)
+				return nil
+			}
+
+			cidrEntries = append(cidrEntries, cidrEntry)
+			cidrEntryMap[cidrEntry.String()] = cidrEntry
+		} else {
+			ipAddrsMap[entry] = net.ParseIP(entry)
+		}
+	}
 
 	inst := ip.VM.CreateObject(ip.Proto)
+
+	inst.Set("getEntries", func(_ js.FunctionCall) js.Value {
+		return ip.VM.ToValue(entries)
+	})
+
+	inst.Set("contains", func(call js.FunctionCall) js.Value {
+		if len(call.Arguments) == 0 {
+			return ip.VM.ToValue(false)
+		}
+
+		entry := call.Argument(0).String()
+
+		if !strings.Contains(entry, "/") {
+			ipEntry := net.ParseIP(entry)
+
+			if _, ok := ipAddrsMap[entry]; ok {
+				return ip.VM.ToValue(true)
+			}
+
+			for _, cidrEntry := range cidrEntries {
+				if cidrEntry.Contains(ipEntry) {
+					return ip.VM.ToValue(true)
+				}
+			}
+
+			return ip.VM.ToValue(false)
+		}
+
+		_, cidrEntry, err := net.ParseCIDR(entry)
+
+		if err != nil {
+			ip.VM.Interrupt(err)
+			return ip.VM.ToValue(false)
+		}
+
+		if _, ok := cidrEntryMap[cidrEntry.String()]; ok {
+			return ip.VM.ToValue(true)
+		}
+
+		return ip.VM.ToValue(false)
+	})
+
+	// inst.Set("append", func(call js.FunctionCall) js.Value {
+
+	// })
 
 	return inst
 }
